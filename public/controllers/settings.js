41eef0913d2d9c38dd875b5561fb0a3f3a9a2126
someone-unknown <poppoppopopo@gmail.com>
2017-08-02 18:56:51 +0300

    v1.2.19

--- public/controllers/settings.js
+++ public/controllers/settings.js
@@ -3,6 +3,38 @@ const View = require('../class/view');
 
 class Settings extends Controller {
 
+    password(){
+        return new Promise((resolve) => {
+            let prompt = $(new View('popup/prompt').toString());
+            $('.ui-dialog-content').dialog('close');
+            prompt.dialog({
+                modal: true,
+                title: 'Enter admin password'
+            });
+            let input = prompt.find('input');
+            input.keyboard();
+            input.focus();
+            prompt.on('submit', (e) => {
+                e.preventDefault();
+
+                let value = input.val();
+                if(value === 'book2017'){
+                    return resolve();
+                }
+                else {
+                    prompt.addClass('error');
+                    prompt.on('animationend', () => {
+                        prompt.removeClass('error');
+                        input.focus();
+                    });
+                    input.val('');
+                }
+                return false;
+
+            });
+        });
+    }
+
     locale(data){
         return new Promise((resolve, reject) => {
             this._app.localization.change(data.locale)
@@ -16,71 +48,79 @@ class Settings extends Controller {
 
     scan(){
         return new Promise((resolve, reject) => {
+            this._app.router.dispatch('settings/password')
+                .then(resolve)
+                .catch(reject);
+        })
+            .then(() => {
 
-            try {
-                let loader = $(new View('popup/loader').toString());
-                $('.ui-dialog-content').dialog('close');
-                loader.dialog({
-                    dialogClass: 'no-header',
-                    modal: true
-                });
-
-                this._app.network.scan()
-                    .then((networks) => {
-
-                        let context = this;
-                        let list = $(new View('popup/wifi', {networks}).toString());
-                        loader.dialog('close');
-                        list.dialog({
-                            title: 'Wi-Fi networks',
+                return new Promise((resolve, reject) => {
+                    try {
+                        let loader = $(new View('popup/loader').toString());
+                        $('.ui-dialog-content').dialog('close');
+                        loader.dialog({
+                            dialogClass: 'no-header',
                             modal: true
                         });
-                        list.on('click', '.wifi-item', function(e){
-                            e.preventDefault();
 
-                            let network = networks[$(this).index()];
-                            let password = $(new View('popup/prompt').toString());
-                            let input = password.find('input');
-                            input.keyboard();
-                            list.dialog('close');
-                            password.dialog({
-                                title: `Enter password for network ${network.ssid}`,
-                                modal: true,
-                                width: '600px'
-                            });
+                        this._app.network.scan()
+                            .then((networks) => {
 
-                            password.on('submit', (e) => {
-                                e.preventDefault();
+                                let context = this;
+                                let list = $(new View('popup/wifi', {networks}).toString());
+                                loader.dialog('close');
+                                list.dialog({
+                                    title: 'Wi-Fi networks',
+                                    modal: true
+                                });
+                                list.on('click', '.wifi-item', function(e){
+                                    e.preventDefault();
 
-                                let value = input.val();
-                                if(value.length < 8) return false;
-                                password.dialog('close');
-                                loader.dialog('open');
-                                context._app.network.connect({
-                                    ssid: network.ssid,
-                                    password: value
-                                })
-                                    .then(() => {
-                                        loader.dialog('close');
-                                    })
-                                    .catch((err) => {
-                                        loader.dialog('close');
-                                        if(err) password.dialog('open');
+                                    let network = networks[$(this).index()];
+                                    let password = $(new View('popup/prompt').toString());
+                                    let input = password.find('input');
+                                    input.keyboard();
+                                    list.dialog('close');
+                                    password.dialog({
+                                        title: `Enter password for network ${network.ssid}`,
+                                        modal: true,
+                                        width: '600px'
                                     });
 
+                                    password.on('submit', (e) => {
+                                        e.preventDefault();
+
+                                        let value = input.val();
+                                        if(value.length < 8) return false;
+                                        password.dialog('close');
+                                        loader.dialog('open');
+                                        context._app.network.connect({
+                                            ssid: network.ssid,
+                                            password: value
+                                        })
+                                            .then(() => {
+                                                loader.dialog('close');
+                                            })
+                                            .catch((err) => {
+                                                loader.dialog('close');
+                                                if(err) password.dialog('open');
+                                            });
+
+                                    });
+                                });
+                            })
+                            .catch((err) => {
+                                $('.ui-dialog-content').dialog('close');
+                                throw err;
                             });
-                        });
-                    })
-                    .catch((err) => {
+                    }
+                    catch(err){
                         $('.ui-dialog-content').dialog('close');
-                        throw err;
-                    });
-            }
-            catch(err){
-                $('.ui-dialog-content').dialog('close');
-                return reject(err);
-            }
-        });
+                        return reject(err);
+                    }
+                });
+
+            });
     }
 
     connect(data){
